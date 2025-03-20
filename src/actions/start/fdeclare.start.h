b7d331c89ff68bb7e182c120ad4ab4dde579c3cf
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2025-03-20 01:45:55 -0300

    tt

--- src/actions/start/fdeclare.start.h
+++ src/actions/start/fdeclare.start.h
@@ -5,6 +5,8 @@
 #include "../../imports/imports.consts.h"
 //silver_chain_scope_end
 
+#define REG_1KB 1000
+#define REG_BUFFER_SIZE REG_1KB * 12
 
 void colect_user_imput(char *input,int max_size);
 
