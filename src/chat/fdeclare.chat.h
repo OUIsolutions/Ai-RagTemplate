958a71099a4b5fe324e04da9228e787bcf7b7577
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-25 08:53:06 -0300

    att

--- src/chat/fdeclare.chat.h
+++ src/chat/fdeclare.chat.h
@@ -12,15 +12,16 @@
 
 #define REG_CHAT_RESPONSE_BUFFER_OK 0
 #define REG_CHAT_RESPONSE_EXIT_COMMAND 1
+#define REG_CHAT_RESPONSE_CLEAR_COMMAND 2
 
 #define REG_CHAT_RESPONSE_GENERIC_ERROR "Unexpected internal error."
 #define REG_CHAT_RESPONSE_BUFFER_BURST_MESSAGE "Input text is too large."
 #define REG_CHAT_RESPONSE_INVALID_READ_MESSAGE "The input text contains invalid characters."
 #define REG_CHAT_RESPONSE_ERROR_ALOCATED_MESSAGE "Error internal in allocated buffer temp."
+#define REG_CHAT_RESPONSE_CLEAR_COMMAND_MESSAGE "The screen has been cleared."
 
 short Reg_init_chat(char *buffer, size_t size_max_buffer, const char *initial_caracter);
 
 const char *Reg_chat_returned_handling(short response_chat);
-
 
 
