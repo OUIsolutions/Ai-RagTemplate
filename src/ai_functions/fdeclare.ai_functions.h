8af72d2720b27bd02abbf64987a5cc75fb594948
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-25 09:04:31 -0300

    att

--- src/ai_functions/fdeclare.ai_functions.h
+++ src/ai_functions/fdeclare.ai_functions.h
@@ -16,18 +16,13 @@ char *agent_list_recursively(cJSON *args, void *pointer);
 void configure_list_recursively_callbacks(OpenAiInterface *openAi);
 
 
-/*
 
-char *agent_list(cJSON *args, void *pointer);
-
-void configure_list_callbacks(OpenAiInterface *openAi);
-
-*/
 char *agent_read_file(cJSON *args, void *pointer);
 
 void configure_read_file_callbacks(OpenAiInterface *openAi);
 
 
 char *agent_write_file(cJSON *args, void *pointer);
+
 
 void configure_write_file_callbacks(OpenAiInterface *openAi);
