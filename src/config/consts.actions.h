2c0ac6c201abaa34f9106d1bbc197ef0467d7093
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-26 19:37:55 -0300

    att

--- src/config/consts.actions.h
+++ src/config/consts.actions.h
@@ -12,5 +12,5 @@ const char *HELP = "help";
 const char *RESSET = "resset";
 const char *LIST_MODEL = "list_models";
 const char *REMOVE_MODEL = "remove_model";
-const char *SET_MODEL_AS_DEFAULT = "set_default_model";
+const char *SET_MODEL_AS_DEFAULT = "set_model_as_default";
 const char *VERSION_ACTION = "version";
