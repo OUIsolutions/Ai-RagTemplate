51922e5dc7ab6363e06385cf892ad36c9d686570
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-26 07:21:21 -0300

    att

--- src/main.c
+++ src/main.c
@@ -10,28 +10,36 @@
 int main(int argc, char  *argv[]){
     start_namespace();
     args_obj  = args.newCArgvParse(argc, argv);
-    int result = -1;
     
     unsigned char *encryption_key = (unsigned char*)malloc(RagCraftkey_size+1);
     RagCraft_get_key(encryption_key);
     encryption = dtw.encryption.newAES_Custom_CBC_v1_interface((char*)encryption_key);
     if(encryption == NULL){
       printf("%sError: %s%s\n", RED, "Invalid encryption key", RESET);
-      goto endmain;
+      release_if_not_null(encryption_key,free);
+      release_if_not_null(config_path,free);
+      return 1;
     }
 
     bool configured = create_user_config_models_path(encryption_key);
     if(!configured){
-      goto endmain;
+      release_if_not_null(encryption_key,free);
+      release_if_not_null(config_path,free);
+      release_if_not_null(encryption,dtw.encryption.free);
+      return 1;
     }
 
     const char *action = args.get_arg(&args_obj, 1);
+    int result = -1;
  
     if(action == NULL){
       printf("%sError: %s%s\n", RED, "No action provided", RESET);
-      result = 1;
-      goto endmain;
+      release_if_not_null(encryption_key,free);
+      release_if_not_null(config_path,free);
+      release_if_not_null(encryption,dtw.encryption.free);
+      return 1;
     }
+
     if(strcmp(action, START) == 0){
       result = start_action();
     }
@@ -63,10 +71,9 @@ int main(int argc, char  *argv[]){
       result = 1;
     }
     
-    endmain:
-      release_if_not_null(encryption_key,free);
-      release_if_not_null(config_path,free);
-      release_if_not_null(encryption,dtw.encryption.free);
-      return result;
+    release_if_not_null(encryption_key,free);
+    release_if_not_null(config_path,free);
+    release_if_not_null(encryption,dtw.encryption.free);
+    return result;
 
 }
